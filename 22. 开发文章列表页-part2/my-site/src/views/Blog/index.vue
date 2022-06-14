9add589bc5fc5d970a8bce63e1cb977ef946d0fe
Sunny-Sea <663253062@qq.com>
2022-06-14 23:52:21 +0800

    开发文章列表页-part2

--- 22. 开发文章列表页-part2/my-site/src/views/Blog/index.vue
+++ 22. 开发文章列表页-part2/my-site/src/views/Blog/index.vue
@@ -1,18 +1,20 @@
 <template>
 	<Layout>
 		<BlogList />
-		<!--<template #right>
-		
-		</template>-->
+		<template #right>
+			<BlogCategory />
+		</template>
 	</Layout>
 </template>
 
 <script>
     import Layout from "@/components/Layout";
+    import BlogCategory from "@/views/Blog/components/BlogCategory";
     import BlogList from "@/views/Blog/components/BlogList";
     
     export default {
         components: {
+            BlogCategory,
             Layout,
             BlogList
         }
