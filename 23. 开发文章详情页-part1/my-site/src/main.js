c5728c5dcbc46cc75d21181c960e1e6f5acc4d67
Sunny-Sea <663253062@qq.com>
2022-06-16 12:49:20 +0800

    开发文章详情页-part1

--- 23. 开发文章详情页-part1/my-site/src/main.js
+++ 23. 开发文章详情页-part1/my-site/src/main.js
@@ -5,13 +5,38 @@ import App from "./App.vue";
 import "./styles/global.less";
 import router from "./router";
 import showMessage from "./utils/showMessage";
+
 Vue.prototype.$showMessage = showMessage;
 
 // 注册全局指令
 import vLoading from "./directives/loading";
-Vue.directive("loading", vLoading);
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
+Vue.directive ( "loading",
+                vLoading );
+
+new Vue ( {
+              router,
+              render: ( h ) => h ( App )
+          } ).$mount ( "#app" );
+
+// 随便测试一下接口
+/* import * as blogApi from "./api/blog";
+
+blogApi.getBlog ( "dadasd" )
+       .then ( ( r ) => {
+           console.log ( r );
+       } );
+
+blogApi.postComment ( {
+                          nickname: "昵称",
+                          connect : "评论内容, 纯文本",
+                          blogId  : "123"
+                      } )
+       .then ( ( r ) => {
+           console.log ( r );
+       } );
+
+blogApi.getComments ( "123456" )
+       .then ( ( r ) => {
+           console.log ( r );
+       } ); */
