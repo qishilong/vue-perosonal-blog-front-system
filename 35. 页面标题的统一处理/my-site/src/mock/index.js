8f4d30a67eb53cdc3de8cc274badd76508ee0699
Sunny-Sea <663253062@qq.com>
2022-06-21 18:58:45 +0800

    页面标题的统一处理

---
+++ 35. 页面标题的统一处理/my-site/src/mock/index.js
@@ -0,0 +1,8 @@
+import "./banner";
+import "./blog";
+import "./setting";
+import Mock from "mockjs";
+
+Mock.setup ( {
+                 timeout: "1000-2000"
+             } );
---
+++ 35. 页面标题的统一处理/my-site/src/store/index.js
@@ -0,0 +1,14 @@
+import Vuex from "vuex";
+import Vue from "vue";
+import banner from "./banner";
+import setting from "./setting";
+
+Vue.use ( Vuex );
+
+export default new Vuex.Store ( {
+                                    modules: {
+                                        banner,
+                                        setting
+                                    },
+                                    strict : true
+                                } );
